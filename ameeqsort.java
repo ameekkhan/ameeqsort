3c7ef16c5930f1012701b5af9897cf7ff0a3d038
Ameeq Hanzlah Sohail <45088332+ameekkhan@users.noreply.github.com>
2020-08-20 08:56:14 +0500

    Update ameeqsort.java

--- ameeqsort.java
+++ ameeqsort.java
@@ -1,31 +1,30 @@
 package sorts.comparison.com;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class AmeeqSort {
 
-	static int temp = 0;
-	static int mid = 0;
+	private static int temp = 0;
+	private static int mid = 0;
+	
 
 	public static void merge(int[] array, int q) {
-
-		if (array.length <= 4) {
+		int size = array.length;
+		 if (size <= 4) {
 			return;
 		}
 
-		else if (array.length % 4 == 0 && q % 2 == 0) {
-			mid = array.length / 2;
+		else if (size % 4 == 0 && q % 2 == 0) {
+			mid = size / 2;
 		}
 
 		else if (q % 2 == 0) {
 			mid = q * 2;
-			mid = array.length - mid;
+			mid = size - mid;
 		}
 
 		else {
 			mid = q * 2 + 2;
-			mid = array.length - mid;
+			mid = size - mid;
 		}
 
 		// Split left part
@@ -36,13 +35,14 @@ public class AmeeqSort {
 		}
 
 		// Split right part
-		int[] right = new int[array.length - mid];
 
-		for (int i = mid; i < array.length; i++) {
+		int[] right = new int[size-mid];
+
+		for (int i = mid; i < size; i++) {
 			right[i - mid] = array[i];
-
-		}
-		merge(left, left.length / 4);
+			}
+		
+		merge(left, mid / 4);
 		merge(right, right.length / 4);
 
 		int i = 0;
@@ -71,79 +71,82 @@ public class AmeeqSort {
 			j++;
 			k++;
 		}
+		
 	}
-
-	public static void main(String[] args) {
-
-int[] numbers= {117, 50, 49, 35, 153, 174, 99, 172, 10, 102, 165, 55, 85, 189, 199, 5, 74, 64, 198, 161, 83, 34, 46, 45, 54, 4, 178};
-
-		int k = numbers.length / 4;
-		int remainder = numbers.length % 4;
-
-		if (remainder >= 2) {
-//sort two elements
-			if (remainder == 2) {
-				if (numbers[0] > numbers[1]) {
-					swap(0, 1, numbers);
-				}
-			}
-//Sort 3 Elements if remainder is 3
-			else {
-				if (numbers[0] > numbers[1])
-					swap(0, 1, numbers);
-				if (numbers[1] > numbers[2])
-					swap(1, 2, numbers);
-				if (numbers[0] > numbers[1])
-					swap(0, 1, numbers);
-
-			}
-
-		}
-	//	long startTime = System.nanoTime();
-		quadrupleSort(numbers, remainder);
-		merge(numbers, k);
-	//	long endTime = System.nanoTime();
-		//long duration = (endTime - startTime);
-		//System.out.println(duration);
-		System.out.println(numbers);
-
-	}
-
-	public static void quadrupleSort(int a[], int remainder) {
-		int j = 0;
-		for (int i = remainder; i < a.length; i = i + 4) {
-			j = i;
-			if (a[i] > a[j + 2]) {
-//a[0] and a[2]
-				swap(i, j + 2, a);
-			}
-
-			if (a[i + 1] > a[j + 3]) {
-// a[1] and a[3]
-				swap(i + 1, j + 3, a);
-			}
-
-			if (a[i] > a[j + 1]) {
-// a[0] and a[1]
-				swap(i, j + 1, a);
-			}
-
-			if (a[i + 2] > a[j + 3]) {
-//a[2] and a[3]
-				swap(i + 2, j + 3, a);
-			}
-			if (a[i + 1] > a[j + 2]) {
-//a[1] and a[2]
-				swap(i + 1, j + 2, a);
-			}
-
-		}
-	}
+	
+	
 
 	public static void swap(int index1, int index2, int numbers[]) {
 		temp = numbers[index1];
 		numbers[index1] = numbers[index2];
 		numbers[index2] = temp;
+	}
+	
+			/* 5 Comparison's To Sort Every Quadruples */
+	public static void quadrupleSort(int[] array,int size,int remainder) {
+		
+	
+		for (int i = remainder; i < size; i = i + 4) {
+			
+			if (array[i] > array[i + 2]) {
+//array[0] and array[2]
+				swap(i, i + 2, array);
+			}
+
+			if (array[i + 1] > array[i + 3]) {
+// array[1] and array[3]
+				swap(i + 1, i + 3, array);
+			}
+
+			if (array[i] > array[i + 1]) {
+// array[0] and array[1]
+				swap(i, i + 1, array);
+			}
+
+			if (array[i + 2] > array[i + 3]) {
+//array[2] and array[3]
+				swap(i + 2, i + 3, array);
+			}
+			if (array[i + 1] > array[i + 2]) {
+//array[1] and array[2]
+				swap(i + 1, i + 2, array);
+				
+			}
+
+		}
+		
+	}
+
+	public static void sort(int[] array) {
+		int size=array.length;
+		int remainder = size % 4;
+		int k = size / 4;
+
+			if (remainder >= 2) {
+				// sort two elements
+				if (remainder == 2) {
+					if (array[0] > array[1]) {
+						swap(0, 1, array);
+					}
+				}
+				// Sort 3 Elements if remainder is 3
+				else {
+					if (array[0] > array[1])
+						swap(0, 1, array);
+					if (array[1] > array[2])
+						swap(1, 2, array);
+					if (array[0] > array[1])
+						swap(0, 1, array);
+				}
+			}
+			
+		/* 5 Comparison's To Sort Every Quadruples */
+			quadrupleSort(array, size,remainder);
+			
+		// Merge All Sorted Quadruples and Remainder Elements
+			merge(array, k);
+		
+
 	}
 
 }
